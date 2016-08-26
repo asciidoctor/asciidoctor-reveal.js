81e1139939498a532344ac32407996ef0757b62e
Olivier Bilodeau <olivier@bottomlesspit.org>
2016-08-25 21:18:45 -0400

    Minimum version check (fixes #64)

--- templates/slim/helpers.rb
+++ templates/slim/helpers.rb
@@ -1,3 +1,14 @@
+require 'asciidoctor'
+require 'json'
+
+if Gem::Version.new(Asciidoctor::VERSION) <= Gem::Version.new('1.5.3')
+  fail 'asciidoctor: FAILED: reveal.js backend needs Asciidoctor >=1.5.4!'
+end
+
+unless defined? Slim::Include
+  fail 'asciidoctor: FAILED: reveal.js backend needs Slim >= 2.1.0!'
+end
+
 # Add custom functions to this module that you want to use in your Slim
 # templates. Within the template you must namespace the function
 # (unless someone can show me how to include them in the evaluation context).
