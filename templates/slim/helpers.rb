fb6e8fbcfcb1570d71426c796d49d586314fa40d
Olivier Bilodeau <olivier@bottomlesspit.org>
2016-09-18 13:14:30 -0400

    Added subtitle partitioning (fixes #70)
    Code taken from Bespoke converter with minor changes

--- templates/slim/helpers.rb
+++ templates/slim/helpers.rb
@@ -9,11 +9,52 @@ unless defined? Slim::Include
   fail 'asciidoctor: FAILED: reveal.js backend needs Slim >= 2.1.0!'
 end
 
-# Add custom functions to this module that you want to use in your Slim
-# templates. Within the template you must namespace the function
-# (unless someone can show me how to include them in the evaluation context).
-# You can change the namespace to whatever you want.
-module Helpers
-  #def self.a_helper_function
-  #end
+# This module gets mixed in to every node (the context of the template) at the
+# time the node is being converted. The properties and methods in this module
+# effectively become direct members of the template.
+module Slim::Helpers
+
+  # Following constants from Bespoke back-end
+  #--
+  EOL = %(\n)
+  SliceHintRx = /  +/
+  #--
+
+  # Following functions are taken from Bespoke back-end
+  #--
+  def pluck selector = {}, &block
+    quantity = (selector.delete :quantity).to_i
+    if blocks?
+      unless (result = find_by selector, &block).empty?
+        result = result[0..(quantity - 1)] if quantity > 0
+        result.each {|b| b.set_attr 'skip-option', '' }
+      end
+    else
+      result = []
+    end
+    quantity == 1 ? result[0] : result
+  end
+
+  def pluck_first selector = {}, &block
+    pluck selector.merge(quantity: 1), &block
+  end
+
+  def partition_title str
+    ::Asciidoctor::Document::Title.new str, separator: (@document.attr 'title-separator')
+  end
+
+  # QUESTION should we wrap in span.line if active but delimiter is not present?
+  # TODO alternate terms for "slice" - part(ition), chunk, segment, split, break
+  def slice_text str, active = nil
+    if (active || (active.nil? && (option? :slice))) && (str.include? '  ')
+      (str.split SliceHintRx).map {|line| %(<span class="line">#{line}</span>) }.join EOL
+    else
+      str
+    end
+  end
+  #--
 end
+
+# More custom functions can be added in another namespace if required
+#module Helpers
+#end
