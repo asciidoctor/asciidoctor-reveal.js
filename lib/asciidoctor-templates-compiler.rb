661c0a736a0e2ec4d4776cd49ef8a0f10c49050f
Guillaume Grossetie <ggrossetie@gmail.com>
2017-08-25 18:24:15 +0200

    Enable Travis build

--- lib/asciidoctor-templates-compiler.rb
+++ lib/asciidoctor-templates-compiler.rb
@@ -4,7 +4,7 @@ module Asciidoctor::TemplatesCompiler
   class RevealjsSlim < Asciidoctor::TemplatesCompiler::Slim
     def engine_options
       ::Asciidoctor::Converter::TemplateConverter::DEFAULT_ENGINE_OPTIONS[:slim].merge(
-        generator: Temple::Generators::ArrayBuffer.new(capture_generate: 'ArrayBuffer')
+        generator: Temple::Generators::ArrayBuffer.new(capture_generator: 'ArrayBuffer')
       )
     end
   end
