249e026f4cd5fec98167ac57249298b32026582f
Guillaume Grossetie <ggrossetie@gmail.com>
2018-03-25 15:07:29 +0200

    Resolves #188, upgrade to Asciidoctor.js 1.5.6-rc.1
    * Remove 'bestikk-opal-compiler' dependency (we now use 'opal-compiler' directly)
    * Remove async dependency (not used anymore)
    * Simplify build scripts and use ECMAScript 6 syntax
    * Run the task examples on Travis
    * The examples task does not run the task build anymore

--- npm/build.js
+++ npm/build.js
@@ -1,4 +1,39 @@
-var Builder = require('./builder.js');
-var builder = new Builder();
+const log = require('bestikk-log');
+const bfs = require('bestikk-fs');
+const CompilerModule = require('./compiler.js');
 
-builder.build();
+const clean = function () {
+  log.task('clean');
+  removeBuildDirSync(); // remove build directory
+};
+
+const removeBuildDirSync = function () {
+  log.debug('remove build directory');
+  bfs.removeSync('build');
+  bfs.mkdirsSync('build');
+};
+
+const compile = function () {
+  CompilerModule.compile();
+};
+
+const copyToDist = function () {
+  log.task('copy to dist/');
+  removeDistDirSync();
+  bfs.copySync('build/asciidoctor-reveal.js', 'dist/main.js');
+};
+
+const removeDistDirSync = function () {
+  log.debug('remove dist directory');
+  bfs.removeSync('dist');
+  bfs.mkdirsSync('dist');
+};
+
+if (process.env.SKIP_BUILD) {
+  log.info('SKIP_BUILD environment variable is true, skipping "build" task');
+  return;
+}
+
+clean();
+compile();
+copyToDist();
