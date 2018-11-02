b57ef24b0666ca558382497dc8f34941103b145c
Guillaume Grossetie <ggrossetie@gmail.com>
2018-11-02 18:32:57 +0100

    Fix the converter registration

--- src/asciidoctor-revealjs.tmpl.js
+++ src/asciidoctor-revealjs.tmpl.js
@@ -5,16 +5,13 @@
 
   var mainModule
 
-  function resolveModule (name) {
+  function resolveModule () {
     if (!mainModule) {
       checkAsciidoctor()
       initialize(Opal)
-      mainModule = Opal.const_get_qualified(Opal.Asciidoctor, 'revealjs') // FIXME: was Html5s
+      mainModule = Opal.const_get_qualified(Opal.Asciidoctor, 'Revealjs')
     }
-    if (!name) {
-      return mainModule
-    }
-    return Opal.const_get_qualified(mainModule, name)
+    return mainModule
   }
 
   function checkAsciidoctor () {
@@ -24,58 +21,20 @@
   }
 
   /**
-   * @param {string} backend The backend name.
-   * @param {Object} opts The converter options.
-   * @return A new instance of `Asciidoctor::Revealjs::Converter`
-   */
-  function Converter (backend, opts) {
-    var converter = resolveModule('Converter').$new(backend, Opal.hash(opts || {}))
-
-    converter.convert = function (node, transform, opts) {
-      return this.$convert(node, transform, Opal.hash(opts || {}))
-    }
-    return converter
-  }
-
-  /**
    * @return {string} Version of this extension.
    */
   function getVersion () {
     return resolveModule().$$const.VERSION.toString()
   }
 
-  // TODO get rid of the colisttreeprocessor if I can
   /**
-   * Registers the Revealjs converter and the supporting extension into Asciidoctor.
-   *
-   * @param registry The Asciidoctor extensions registry to register the
-   *   extension into. Defaults to the global Asciidoctor registry.
-   * @throws {TypeError} if the *registry* is invalid or Asciidoctor.js is not loaded.
+   * Registers the reveal.js converter.
    */
-  function register (registry) {
-    if (!registry) {
-      checkAsciidoctor()
-      registry = Opal.Asciidoctor.Extensions
-    }
-    /*
-    var processor = AttachedColistTreeprocessor()
-
-    // global registry
-    if (typeof registry.register === 'function') {
-      registry.register(function () {
-        this.treeProcessor(processor)
-      })
-    // custom registry
-    } else if (typeof registry.block === 'function') {
-      registry.treeProcessor(processor)
-    } else {
-      throw new TypeError('Invalid registry object')
-    }
-    */
+  function register () {
+    return resolveModule()
   }
 
   var facade = {
-    Converter: Converter,
     getVersion: getVersion,
     register: register,
   }
--- test/smoke.js
+++ test/smoke.js
@@ -1,23 +1,26 @@
-const asciidoctor = require('asciidoctor.js')();
-const asciidoctorRevealJs = require('../dist/main.js');
-//require('../dist/main.js');
+const asciidoctor = require('asciidoctor.js')()
+const asciidoctorRevealjs = require('../dist/main.js')
+//require('../dist/main.js')
 
-const expect = require('expect.js');
+const expect = require('expect.js')
 
-// Register the RevealJs converter
-asciidoctorRevealJs.register()
+// Register the reveal.js converter
+const revealjsConverter = asciidoctorRevealjs.register()
+const version = revealjsConverter.getVersion();
 
-const attributes = {'revealjsdir': 'node_modules/reveal.js@'};
-const options = {safe: 'safe', backend: 'revealjs', attributes: attributes, 'header_footer': true};
+const attributes = {'revealjsdir': 'node_modules/reveal.js@'}
+const options = {safe: 'safe', backend: 'revealjs', attributes: attributes, 'header_footer': true}
 const content = `= Title Slide
 
 == Slide One
 
 * Foo
 * Bar
-* World`;
+* World`
 
-const result = asciidoctor.convert(content, options);
+const result = asciidoctor.convert(content, options)
 
-expect(result).to.contain('<script src="node_modules/reveal.js/js/reveal.js">');
-expect(result).to.contain('<li><p>Foo</p></li>');
+expect(version).to.be('1.5.6')
+expect(result).to.contain('<script src="node_modules/reveal.js/js/reveal.js">')
+expect(result).to.contain('<li><p>Foo</p></li>')
+
