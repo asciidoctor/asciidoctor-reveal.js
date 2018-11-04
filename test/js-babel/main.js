52c4e3feed5c731091ab4bc4c9a98a3b98abb5df
Olivier Bilodeau <olivier@bottomlesspit.org>
2018-11-04 00:58:42 -0400

    Tests: test/js-babel/ now works and highlights the change we made
    Also allows us to retest in the future and not forget about that use-case.

--- test/js-babel/main.js
+++ test/js-babel/main.js
@@ -1,12 +1,14 @@
 import Asciidoctor from 'asciidoctor.js'
 const asciidoctor = Asciidoctor()
 
+// BEFORE SUPPORT
 // Babel will move the import statement at the top of the file.
 // As a consequence, the reveal.js converter will be loaded before the initialization of Asciidoctor.js.
-import 'asciidoctor-reveal.js'
-// If we replace the import statement by a require statement then it's working.
-// Another way to fix this issue is to lazily load the reveal.js converter.
-//require('asciidoctor-reveal.js')
+//import 'asciidoctor-reveal.js'
+
+// NOW SUPPORTED
+import AsciidoctorRevealJs from 'asciidoctor-reveal.js'
+AsciidoctorRevealJs.register()
 
 const attributes = { revealjsdir: 'node_modules/reveal.js@' }
 const options = {
