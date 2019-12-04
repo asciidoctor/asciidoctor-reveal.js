51a32ca7e4b8a43ffa8025d2ced78f03536f1c8e
Olivier Bilodeau <olivier@bottomlesspit.org>
2019-12-04 09:28:09 -0500

    Update to Babel 7.7.4 (fix GitHub security warnings)
    API usage is following the project's 1.x API

--- test/js-babel/main.js
+++ test/js-babel/main.js
@@ -1,18 +1,23 @@
 import Asciidoctor from 'asciidoctor.js'
 const asciidoctor = Asciidoctor()
 
-// BEFORE SUPPORT
 // Babel will move the import statement at the top of the file.
 // As a consequence, the reveal.js converter will be loaded before the initialization of Asciidoctor.js.
 //import 'asciidoctor-reveal.js'
 
-// NOW SUPPORTED
-import AsciidoctorRevealjs from 'asciidoctor-reveal.js'
-AsciidoctorRevealjs.register()
+// To workaround this issue, we use replace the import statement by a require statement.
+require('asciidoctor-reveal.js')
 
+// Please note that asciidoctor-reveal.js 2.0.0 supports lazy loading.
+// The following should work with asciidoctor-reveal.js 2.0+
+//import AsciidoctorReveal from 'asciidoctor-reveal.js'
+//AsciidoctorReveal.register()
+
+const attributes = { revealjsdir: 'node_modules/reveal.js@' }
 const options = {
   safe: 'safe',
   backend: 'revealjs',
+  attributes: attributes,
   header_footer: true
 }
 
