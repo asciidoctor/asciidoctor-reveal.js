1222b00684628d9aa8286732351440524153723f
Guillaume Grossetie <ggrossetie@gmail.com>
2019-12-09 09:08:24 +0100

    resolves #261 use the new syntax highlighter API introduced in Asciidoctor 2.0.0

--- lib/asciidoctor-revealjs.rb
+++ lib/asciidoctor-revealjs.rb
@@ -1,8 +1,10 @@
 if RUBY_ENGINE == 'opal'
   require 'asciidoctor-revealjs/converter'
   require 'asciidoctor-revealjs/version'
+  require 'asciidoctor-revealjs/highlightjs'
 else
   require 'asciidoctor' unless defined? Asciidoctor::Converter
   require_relative 'asciidoctor-revealjs/converter'
   require_relative 'asciidoctor-revealjs/version'
+  require_relative 'asciidoctor-revealjs/highlightjs'
 end
